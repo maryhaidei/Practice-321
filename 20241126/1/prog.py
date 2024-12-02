c17c91bac0f5b34a75db5b6060031acda66418f7
Mary <maraihaidei03@gmail.com>
2024-12-02 22:52:44 +0300

    hw

--- 20241126/1/prog.py
+++ 20241126/1/prog.py
@@ -1,5 +1,8 @@
 import sys
-s=sys.stdin.read(); n=ord(s[0]); l=len(s)-1; ss=s[1:]
-le=[ss[i*int(l/n): (i+1)*int(l/n)] if i!= n-1 else ss[i*int(l/n):] for i in range(n)] if n < l else sorted(ss);
-st=s[0]+''.join(sorted(le))
-for i in st:sys.stdout.buffer.write(bytes(ord(i)))
+s=sys.stdin.buffer.read(); n=s[0]; l=len(s)-1; ss=s[1:-1]
+le=sorted([ss[i*int(l/n): (i+1)*int(l/n)] if i!= n-1 else ss[i*int(l/n):] for i in range(n)]) if n < l else sorted(ss);
+sys.stdout.buffer.write(bytes([s[0]]))
+try:
+    for t in le: sys.stdout.buffer.write(t)
+except Exception:
+    sys.stdout.buffer.write(bytes(list(le)))
--- 20241126/2/prog.py
+++ 20241126/2/prog.py
@@ -1,3 +1,3 @@
 import sys
 
-sys.stdout.write(sys.stdin.read().encode('latin1',  errors="replace").decode('cp1251',  errors="replace"))
+sys.stdout.write(sys.stdin.read().encode('latin1',  errors="replace").decode('cp1251'))
