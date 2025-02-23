b67fd32993e5ce18d3b850d88da1bb9292812816
Mary <maraihaidei03@gmail.com>
2025-02-23 21:50:44 +0300

    second part of ex. finding object, get object and decode(encode) object

--- 20250217/1/prog.py
+++ 20250217/1/prog.py
@@ -1,4 +1,5 @@
 import os
+import sys 
 
 def show_git_branches(git_folder):
     heads_dir = os.path.join(git_folder, "refs", "heads")
@@ -8,4 +9,16 @@ def show_git_branches(git_folder):
     for root, _, files in os.walk(heads_dir):
         for file in files:
             print(os.path.relpath(os.path.join(root, file), heads_dir))
-
+            
+def load_object(repo_dir, obj_id):
+    obj_path = os.path.join(repo_dir, ".git", "objects", obj_id[:2], obj_id[2:])
+    try:
+        with open(obj_path, "rb") as file:
+            compressed_data = file.read()
+    except FileNotFoundError:
+        sys.exit(f"Не найден объект {obj_id}")
+    decompressed_data = zlib.decompress(compressed_data)
+    header, _, data = decompressed_data.partition(b'\x00')
+    header_text = header.decode("utf-8")
+    obj_type, _ = header_text.split(" ", 1)
+    return obj_type, data
