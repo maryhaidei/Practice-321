bcfaa489e30913616ea6cc686fa6e1d1e543528d
Mary <maraihaidei03@gmail.com>
2025-03-01 23:33:35 +0300

    finish class MUD, add function to play the game

--- 20250224/1/prog.py
+++ 20250224/1/prog.py
@@ -6,7 +6,7 @@ class Gamer:
     def move(self, x, y): 
         a, b =self.cord
         if y in ['left', 'right']: self.cord=(a+x if a>0 and a<9 else 0 if a+x>9 else 9, b)
-        if y in ['up', 'down']: self.cord=(a, b+x if b>0 and b<9 else 0 if b+x>9 else 9)
+        else: self.cord=(a, b+x if b>0 and b<9 else 0 if b+x>9 else 9)
         print("Moved to", self.cord)
     
 class Monster: 
@@ -27,10 +27,26 @@ class MUD:
         pl=Gamer(); direct=['right', 'left', 'up', 'down']
         while s:=input():
             s=s.strip().split()
-            if any([True for i in direct if i in s]): 
+            if [True for i in direct if i==s[0]]: 
                 if len(s)>1: 
                     print("Invalid arguments");continue 
-                pl
+                pl.move(1 if s[0] in ['right', 'down'] else -1, s[0])
+                x, y = pl.cord
+                if self.monsters[x][y]!=0: 
+                    self.monsters[x][y].say()
+            if 'addmon'==s[0]: 
+                if len(s)!=4: 
+                    print("Invalid arguments");continue 
+                try: 
+                    self.monsters[s[1]][s[2]]=Monster(s[1], s[2], s[3])
+                    replaced= True if self.monsters[s[1]][s[2]] !=0 else False
+                except: 
+                    print("Invalid arguments");continue
+                if replaced: 
+                    print("Replaced the old monster")
+                    replaced=False 
+            else: 
+                print("Invalid command")
                 
                 
                 
