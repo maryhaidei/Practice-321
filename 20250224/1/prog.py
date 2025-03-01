625daa75f6961745d0ced6c8e9c2fab8cc868ba5
Mary <maraihaidei03@gmail.com>
2025-03-01 22:13:24 +0300

    add class Gamer and its functionality

---
+++ 20250224/1/prog.py
@@ -0,0 +1,22 @@
+import cowsay
+
+class Gamer:
+    def __init__(self): 
+        self.cord=(0,0)
+    def down(self): 
+        a, b =self.cord
+        self.cord= (a, b+1 if b<9 else 0)
+        print("Moved to", self.cord)
+    def up(self): 
+        a, b=self.cord
+        self.cord = (a, b-1 if b>0 else 9)
+        print("Moved to", self.cord)
+    def left(self): 
+        a, b =self.cord
+        self.cord= (a-1 if a>0 else 9, b)
+        print("Moved to", self.cord)
+    def right(self): 
+        a, b=self.cord
+        self.cord = (a+1 if a<9 else 0, b)
+        print("Moved to", self.cord)
+    
