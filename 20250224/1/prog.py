44ea8a41ad13fe000c13decced49f9dc0b088b3e
Mary <maraihaidei03@gmail.com>
2025-03-01 23:16:57 +0300

    correct class Gamer and add function move

--- 20250224/1/prog.py
+++ 20250224/1/prog.py
@@ -3,21 +3,10 @@ import cowsay
 class Gamer:
     def __init__(self): 
         self.cord=(0,0)
-    def down(self): 
+    def move(self, x, y): 
         a, b =self.cord
-        self.cord= (a, b+1 if b<9 else 0)
-        print("Moved to", self.cord)
-    def up(self): 
-        a, b=self.cord
-        self.cord = (a, b-1 if b>0 else 9)
-        print("Moved to", self.cord)
-    def left(self): 
-        a, b =self.cord
-        self.cord= (a-1 if a>0 else 9, b)
-        print("Moved to", self.cord)
-    def right(self): 
-        a, b=self.cord
-        self.cord = (a+1 if a<9 else 0, b)
+        if y in ['left', 'right']: self.cord=(a+x if a>0 and a<9 else 0 if a+x>9 else 9, b)
+        if y in ['up', 'down']: self.cord=(a, b+x if b>0 and b<9 else 0 if b+x>9 else 9)
         print("Moved to", self.cord)
     
 class Monster: 
@@ -27,3 +16,22 @@ class Monster:
         print("Added monster to", self.cord, "saying", self.hello)
     def say(self): 
         print(cowsay.cowsay(self.hello))
+        
+class MUD: 
+    def __init__(self): 
+        self.monsters=[[0 for _ in range(10)] for _ in range(10)]
+    
+    def encounter(self, x, y): 
+        self.monsters[x][y].say()
+    def play(self): 
+        pl=Gamer(); direct=['right', 'left', 'up', 'down']
+        while s:=input():
+            s=s.strip().split()
+            if any([True for i in direct if i in s]): 
+                if len(s)>1: 
+                    print("Invalid arguments");continue 
+                pl
+                
+                
+                
+        
